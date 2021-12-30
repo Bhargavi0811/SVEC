caae576fb3f2107d681c6464fb8ee2a2cf7c7c9d
Bhargavi0811 <madinibharu@gmail.com>
2021-12-30 12:21:19 +0530

    ReadMe.md file are added

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -35,24 +35,65 @@
 
 //Array
 
-// let employees=["Sujith","Bhaaru"]
+// let employees=["Sujith","Swetha","Bhaaru"]
 // console.log(employees[1])
 
 //Alerts
 
-// alert("your registration is completed")
+// alert("Registration is completed")
+// prompt("Enter user name")
 
-// // console statements
-//  console.log("welcome");
-//  console.info("for information");
-//  console.warn("check the code once");
+// number1=prompt("ENter number")
+// console.log(number1)
 
-// spread operator
+// confirm("Are u sure to exit?")
 
-let external_marks=[23,33,23,43,56,57]
-let internal_marks=[12,4,3,6,7,15]
+//console statements
 
-let results=[2334,external_marks]
-let final=[23,445,556,...external_marks,...internal_marks]
-console.log(results)
-console.log(final)
+// console.log("Swetha");
+// console.info("For info");
+// console.warn("check the code once");
+// console.error("name is not defined");
+
+//Spread Operators
+
+// let external_marks=[23,45,67,80,64,90]
+// let internal_marks=[20,20,17,14,18,19]
+
+// let results=[23,34,external_marks]
+// let final=[23,445,556,...external_marks,...internal_marks]
+// console.log(results)
+// console.log(final)
+//rest parameter
+// function add(x,y,...remainData){
+//     console.log(x)
+//     console.log(y)
+//     console.log(remainData)
+//     console.log(typeof(remainData))
+// }
+
+// console.log(add(19,19,45,56576,7877))
+
+//Destructuring of Array
+//  let employees=["Bhaaru","Divya","Swetha"]
+// let name1=employees[0];
+// let name2=employees[1];
+
+// let[Bhaaru,Divya,...data]=employees
+//  console.log(name1)
+//  console.log(name2)
+
+// console.log(data)
+//Destructuring of object
+
+let employee={
+    uname:"Sujith",
+     designation:"Junior Doctor",
+     salary:"12 LPA",
+     mobile:9039023902
+ }
+ let{uname,designation,...remainList}=employee;
+ console.log(uname);
+ console.log(designation);
+ console.log(remainList);
+
